a7804145e63668a23a62a60624c7557418539b1a
Vatsal Joshi <vatz88@gmail.com>
2016-10-26 09:33:23 +0530

    home page updated

--- server.js
+++ server.js
@@ -8,21 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/vz_style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'vz_style.css'));
 });
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/vz_script.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'vz_script.js'));
 });
 
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'articles', 'article-one.html'));
 });
 
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+  console.log(`VzBlog running! on port 8080`);
 });
