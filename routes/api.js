7442b55cfcab7bdfc41385ce7d7e89ce443cffe7
Vatsal Joshi <vatz88@gmail.com>
2016-11-17 01:17:21 +0530

    api server-side validation bug fixed

--- routes/api.js
+++ routes/api.js
@@ -5,13 +5,13 @@ var pool = app.get('pool');
 // routes
 
 // user profile by id
-router.get('/api/user/:id', function(req, res) {
-    pool.connect(function(err, client, done) {
+router.get('/api/user/:id', function (req, res) {
+    pool.connect(function (err, client, done) {
         if (err) {
             res.status(500).send(err.toString());
         }
         else {
-            client.query('SELECT * FROM user_details WHERE user_id = $1', [req.params.id], function(err, result) {
+            client.query('SELECT * FROM user_details WHERE user_id = $1', [req.params.id], function (err, result) {
                 done();
                 if (err) {
                     res.status(500).send(err.toString());
@@ -23,13 +23,13 @@ router.get('/api/user/:id', function(req, res) {
 });
 
 // get all articles
-router.get('/api/articles/all', function(req, res) {
-    pool.connect(function(err, client, done) {
+router.get('/api/articles/all', function (req, res) {
+    pool.connect(function (err, client, done) {
         if (err) {
             res.status(500).send(err.toString());
         }
         else {
-            client.query('SELECT * FROM articles, user_details WHERE user_details.user_id = articles.user_id ORDER BY "article_id" ASC', function(err, result) {
+            client.query('SELECT * FROM articles, user_details WHERE user_details.user_id = articles.user_id ORDER BY "article_id" ASC', function (err, result) {
                 done();
                 if (err) {
                     res.status(500).send(err.toString());
@@ -41,13 +41,13 @@ router.get('/api/articles/all', function(req, res) {
 });
 
 // get article_name, article_content, first_name, last_name, articles.user_id by id
-router.get('/api/articles/user/:user', function(req, res) {
-    pool.connect(function(err, client, done) {
+router.get('/api/articles/user/:user', function (req, res) {
+    pool.connect(function (err, client, done) {
         if (err) {
             res.status(500).send(err.toString());
         }
         else {
-            client.query('SELECT article_name, article_content, first_name, last_name, articles.user_id FROM articles, user_details WHERE user_details.user_id = articles.user_id AND user_details.user_id = $1', [req.params.user], function(err, result) {
+            client.query('SELECT article_name, article_content, first_name, last_name, articles.user_id FROM articles, user_details WHERE user_details.user_id = articles.user_id AND user_details.user_id = $1', [req.params.user], function (err, result) {
                 done();
                 if (err) {
                     res.status(500).send(err.toString());
@@ -59,13 +59,13 @@ router.get('/api/articles/user/:user', function(req, res) {
 });
 
 // get article_name, article_content and author's first_name, last_name by tag
-router.get('/api/articles/tag/:tag', function(req, res) {
-    pool.connect(function(err, client, done) {
+router.get('/api/articles/tag/:tag', function (req, res) {
+    pool.connect(function (err, client, done) {
         if (err) {
             res.status(500).send(err.toString());
         }
         else {
-            client.query('SELECT article_name, article_content, first_name, last_name FROM user_details, (SELECT article_name, article_content, user_id AS uid FROM articles, (SELECT article_id AS artiID FROM article_tags WHERE UPPER(article_tags.tag) = UPPER($1)) AS tableArtiID WHERE article_id = artiID) AS tableArticle WHERE uid = user_details.user_id', [req.params.tag], function(err, result) {
+            client.query('SELECT article_name, article_content, first_name, last_name FROM user_details, (SELECT article_name, article_content, user_id AS uid FROM articles, (SELECT article_id AS artiID FROM article_tags WHERE UPPER(article_tags.tag) = UPPER($1)) AS tableArtiID WHERE article_id = artiID) AS tableArticle WHERE uid = user_details.user_id', [req.params.tag], function (err, result) {
                 done();
                 if (err) {
                     res.status(500).send(err.toString());
@@ -77,32 +77,35 @@ router.get('/api/articles/tag/:tag', function(req, res) {
 });
 
 // Search entire blog by keywords
-router.get('/api/searchBlog', function(req, res) {
-    var keywords = req.query.keywords.split(' ');
-    pool.connect(function(err, client, done) {
+router.get('/api/searchBlog', function (req, res) {
+    var keywords = req.query.keywords.split(/\s|\'|%|\"|;/g);
+    console.log(keywords);
+    pool.connect(function (err, client, done) {
         if (err) {
             res.status(500).send(err.toString());
         }
         else {
             var query = '';
             for (var i = 0, len = keywords.length; i < len; i++) {
-                query = query +
-                    "(" +
-                    "SELECT * " +
-                    "FROM articles, user_details " +
-                    "WHERE user_details.user_id = articles.user_id AND " +
-                    "(" +
-                    "UPPER(articles.tag) like UPPER('%" + encodeURIComponent(keywords[i]) + "%') OR " +
-                    "UPPER(articles.article_name) like UPPER('%" + encodeURIComponent(keywords[i]) + "%') OR " +
-                    "UPPER(user_details.first_name) like UPPER('%" + encodeURIComponent(keywords[i]) + "%') OR " +
-                    "UPPER(user_details.last_name) like UPPER('%" + encodeURIComponent(keywords[i]) + "%')" +
-                    ")" +
-                    ")";
-                if (i != len - 1) {
-                    query = query + 'UNION';
+                if (keywords[i] !== "") {
+                    if (query !== '') {
+                        query = query + 'UNION';
+                    }
+                    query = query +
+                        "(" +
+                        "SELECT * " +
+                        "FROM articles, user_details " +
+                        "WHERE user_details.user_id = articles.user_id AND " +
+                        "(" +
+                        "UPPER(articles.tag) like UPPER('%" + (keywords[i]) + "%') OR " +
+                        "UPPER(articles.article_name) like UPPER('%" + (keywords[i]) + "%') OR " +
+                        "UPPER(user_details.first_name) like UPPER('%" + (keywords[i]) + "%') OR " +
+                        "UPPER(user_details.last_name) like UPPER('%" + (keywords[i]) + "%')" +
+                        ")" +
+                        ")";
                 }
             }
-            client.query(query, function(err, result) {
+            client.query(query, function (err, result) {
                 done();
                 if (err) {
                     res.status(500).send(err.toString());
